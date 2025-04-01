653a364c8c9fa7a80f49b0f050d52ef06bd654d8
Tram19 <87849552+Tram19@users.noreply.github.com>
2025-04-01 22:51:08 +0700

    Update test_widget.c

--- samples/sample_c/module_sample/widget/test_widget.c
+++ samples/sample_c/module_sample/widget/test_widget.c
@@ -337,10 +337,12 @@ static void *DjiTest_WidgetTask(void *arg)
         
 #ifndef USER_FIRMWARE_MAJOR_VERSION
         uint32_t debug_heap_use = configTOTAL_HEAP_SIZE - xPortGetFreeHeapSize();
+        static uint32_t max_heap_use=0;
+        if(debug_heap_use>max_heap_use) {max_heap_use=debug_heap_use;)
         snprintf(message, DJI_WIDGET_FLOATING_WINDOW_MSG_MAX_LEN, "System Status: %s\r\n%d/%d\r\n"
                                                                 , systemStatus
                                                                 , debug_heap_use
-                                                            , configTOTAL_HEAP_SIZE);
+                                                            , max_heap_use);
         
 //        snprintf(message, DJI_WIDGET_FLOATING_WINDOW_MSG_MAX_LEN, "System Status: %s\r\n", systemStatus);
 #else
